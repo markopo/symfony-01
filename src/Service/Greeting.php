1158f0fb25b1cbed7144ce28a3bb021a9600a9f4
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-12-02 18:47:06 +0100

    Services

--- src/Service/Greeting.php
+++ src/Service/Greeting.php
@@ -18,20 +18,25 @@ class Greeting
      * @var LoggerInterface
      */
     private $logger;
+    /**
+     * @var string
+     */
+    private $message;
 
 
     /**
      * Greeting constructor.
      * @param LoggerInterface $logger
      */
-    public function __construct(LoggerInterface $logger)
+    public function __construct(LoggerInterface $logger, string $message)
     {
         $this->logger = $logger;
+        $this->message = $message;
     }
 
     public function greet(string $name): string {
         $this->logger->info("Greeted $name");
-        return "Hello $name";
+        return "{$this->message} $name";
     }
 
 }

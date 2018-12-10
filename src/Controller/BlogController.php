3926a680bb7021e01812eb148c4aa36f317ab9a9
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-12-10 20:32:25 +0100

    Add blog routes & views

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,22 +11,43 @@ namespace App\Controller;
 
 use App\Service\Greeting;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
+/**
+ * // http://symfony-01.local/blog/marko
+ * @Route("/blog") 
+ */
 class BlogController extends AbstractController
 {
 
 
     private $greeting;
-
     /**
-     * BlogController constructor.
+     * @var SessionInterface
      */
-    public function __construct(Greeting $greeting)
+    private $session;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+
+    /***
+     * BlogController constructor.
+     * @param Greeting $greeting
+     * @param SessionInterface $session
+     * @param RouterInterface $router
+     */
+    public function __construct(Greeting $greeting, SessionInterface $session, RouterInterface $router)
     {
         $this->greeting = $greeting;
-
+        $this->session = $session;
+        $this->router = $router;
     }
 
     /**
@@ -34,9 +55,35 @@ class BlogController extends AbstractController
      */
     public function index(Request $request) {
 
-        $name = $request->get('name');
-        $message = $name !== null ? $this->greeting->greet($name) : '';
 
-        return $this->render('base.html.twig', [ 'message' => $message  ]);
+        return $this->render('blog/index.html.twig', [ 'posts' => $this->session->get('posts') ]);
+    }
+
+    /**
+     * @Route("/add", name="blog_add")
+     */
+    public function add() {
+        $posts = $this->session->get('posts');
+        $posts[uniqid()] = [
+            'title' => 'A random title ' . rand(1, 5000),
+            'text' => 'Some random text nr: '. rand(1, 5000)
+        ];
+
+        $this->session->set('posts', $posts);
+
+        return new RedirectResponse($this->router->generate('blog_index'));
+    }
+
+    /**
+     * @Route("/show/{id}", name="blog_show")
+     */
+    public function show($id) {
+        $posts = $this->session->get('posts');
+
+        if(!$posts || !isset($posts[$id])){
+            throw new NotFoundHttpException('Post not found!');
+        }
+
+        return $this->render('blog/post.html.twig', [ 'id' => $id, 'post' => $posts[$id] ]);
     }
 }
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markopoikkimaki
+ * Date: 2018-12-08
+ * Time: 09:04
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class HomeController  extends AbstractController
+{
+
+
+    /**
+     * HomeController constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @Route("/", name="home_index")
+     */
+    public function index(Request $request) {
+
+
+      return $this->render('base.html.twig', [ 'message' => 'Welcome to Home Page!' ]);
+    }
+}

ac9ea78df2d53f963a74fee874acea0dd5e52509
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-12-15 10:29:02 +0100

    MicroPost
    php bin/console make:entity MicroPost
    
    php bin/console doctrine:migrations:diff
    
    php bin/console doctrine:migrations:migrate

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -16,8 +16,48 @@ class MicroPost
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+
+
+
+
+
 }
---
+++ src/Migrations/Version20181215092423.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181215092423 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE micro_post ADD text LONGTEXT NOT NULL, ADD time DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE micro_post DROP text, DROP time');
+    }
+}

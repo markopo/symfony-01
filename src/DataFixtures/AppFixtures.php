ad7da59f103c038fea3b1d0b853f0ad3990a776d
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-12-26 15:16:43 +0100

    User - app fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,13 +3,27 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     private $faker;
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
 
     public function load(ObjectManager $manager)
     {
@@ -20,8 +34,25 @@ class AppFixtures extends Fixture
 
         $this->loadMicroPosts($manager);
 
+        $this->loadUsers($manager);
 
         $manager->flush();
+    }
+
+    private function loadUsers(ObjectManager $manager): void {
+
+            for($i=0;$i<20;$i++) {
+
+                $user = new User();
+                $user->setEmail($this->faker->email);
+                $user->setUsername($this->faker->userName);
+                $user->setFullName($this->faker->name);
+                $user->setPassword($this->passwordEncoder->encodePassword($user, 'abc123'));
+
+                $manager->persist($user);
+            }
+
+
     }
 
     private function loadMicroPosts(ObjectManager $manager): void {

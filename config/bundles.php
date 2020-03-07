bb9be6322b88de0da65e3500930795107a2a5acb
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2020-03-07 11:09:48 +0100

    Composer require api
    https://api-platform.com/docs/

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Cron\CronBundle\CronCronBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
